f190f4c48df68a0438231751206e355046613b09
Paolo Baiada <89268273+paolobaiada@users.noreply.github.com>
2022-12-01 13:02:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector('.bx-menu');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click',function(){
+navbar.classList.toggle('active');
+}) ;
+    
